68bdd545aa9f11abc7bfe492a183cfdf18485570
Rithvik.Velapati <rithvik.velapati@thehealthvice.com>
2024-09-12 08:13:32 -0500

    Bottom Bar

--- frontend/wellfed/src/app/layout.tsx
+++ frontend/wellfed/src/app/layout.tsx
@@ -1,3 +1,4 @@
+// src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -11,12 +12,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} {/* This will render the home page and BottomBar */}
+      </body>
     </html>
   );
 }
