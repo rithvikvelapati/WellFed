cbb0a14cde10fbba5c66f5d48bfe74a9e8c66196
Ankita Sahoo <ankitasahoo.190199@gmail.com>
2024-09-16 12:44:12 -0500

    Features of Side Bar added.

--- frontend/wellfed/src/components/Sidebar.tsx
+++ frontend/wellfed/src/components/Sidebar.tsx
@@ -3,11 +3,9 @@ import Image from "next/image";
 const Sidebar = () => {
   return (
     <div className="bg-white h-full w-20 flex flex-col items-center py-4">
-
-      <div className="mb-16">
-        <Image src="/Logo.svg" alt="Logo" width={100} height={100} className="w-10 h-10 logo" />
+      <div className="mb-14">
+        <Image src="/Logo.svg" alt="Logo" width={100} height={100} className="w-15 h-15 logo" />
       </div>
-
       <nav className="flex flex-col space-y-10">
         <Image src="/Home.svg" alt="Home" width={100} height={100} className="w-6 h-6 text-gray-600" />
         <Image src="/AddFriend.svg" alt="AddUser" width={100} height={100} className="w-6 h-6 text-gray-600" />
