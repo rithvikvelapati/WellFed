fa05a5b51e3c584f96cd7546c55b3a7963044afc
Ankita Sahoo <ankitasahoo.190199@gmail.com>
2024-09-16 12:23:21 -0500

    Features of Top Bar added.

--- frontend/wellfed/src/app/layout.tsx
+++ frontend/wellfed/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TopBar from '../components/Topbar';
+import Sidebar from '../components/Sidebar';
 import './globals.css';
 
 export const metadata = {
@@ -15,11 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex h-screen overflow-hidden">
-
+          <div className="fixed top-0 left-0 h-full w-20 z-10">
+            <Sidebar />
+          </div>
           <div className="fixed top-0 left-20 right-0 h-16 z-10">
               <TopBar />
           </div>
-
         </div>
       </body>
     </html>
