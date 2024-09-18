7d89184186413bc3ebbe614f128cbd86000d4762
Diego Medina <romeomdna@outlook.com>
2024-09-18 11:22:19 -0400

    tailwindcss configuration update

--- frontend/wellfed/postcss.config.mjs
+++ frontend/wellfed/postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-  },
+    "postcss-import": [require("tailwindcss"), require("autoprefixer")]
+  }
 };
 
 export default config;
