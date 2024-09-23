1d0a8ef76644c6f68fa46a4e3d45bde197e9e5db
Rithvik.Velapati <rithvik.velapati@thehealthvice.com>
2024-09-23 13:15:40 -0500

    Mobile Calendar Home Page

--- frontend/wellfed/src/app/page.tsx
+++ frontend/wellfed/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DashboardContent from "../components/DashboardContent";
 import '../app/globals.css';
+import EditEvent from './components/EditEvent/EditEvent';
+import Calendar from './components/EventCalender/Calender';
 
 const Page: React.FC = () => {
   return (
     <div className="p-4">
       {/* This page doesn't do anything for now */}
-      <DashboardContent/>
+      <Calendar />
     </div>
   );
 };
