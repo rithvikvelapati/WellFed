194d372bfe8d47a4c995d2e4ce6aa0baf3c15fa2
Diego Medina <romeomdna@outlook.com>
2024-09-27 11:34:54 -0400

    added filter button and invite button with pre-functionality

--- frontend/wellfed/src/app/food-section/recipe-list/ui/ToggleButtonGroup.tsx
+++ frontend/wellfed/src/app/food-section/recipe-list/ui/ToggleButtonGroup.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { IoIosAdd } from 'react-icons/io';
 
 interface ToggleButtonGroupProps {
   // Optional props can be added here in the future
@@ -8,6 +10,7 @@ interface ToggleButtonGroupProps {
 
 const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = () => {
   const [selected, setSelected] = useState('Self');
+  const router = useRouter();
 
   const buttons = ['Self', 'Family'];
 
@@ -16,23 +19,38 @@ const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = () => {
     // Additional logic can be added here when focusing on functionality
   };
 
+  const handleAddButtonClick = () => {
+    // Navigate to the modal overlay page
+    router.push('/add-member'); // Adjust the route as needed
+  };
+
   return (
-    <div className="flex justify-center space-x-fluid-px mb-6">
+    <div className="flex justify-center space-x-4 mb-6">
       {buttons.map((button) => (
         <button
           key={button}
           onClick={() => handleButtonClick(button)}
-          className={`w-24 h-[36px] rounded-3xl flex items-center justify-center font-normal text-fluid-sm shadow-lg
+          className={`w-24 h-9 rounded-3xl flex items-center justify-center font-normal text-sm shadow-lg
             ${
               selected === button
                 ? 'bg-gradient-to-r from-primary to-secondary text-white'
                 : 'bg-slate-200 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent'
             }
-            focus:outline-none focus:ring-2 focus:ring-gradient`}
+            focus:outline-none focus:ring-2 focus:ring-secondary`}
         >
-          <span className="text-sm font-medium">{button}</span>
+          <span>{button}</span>
         </button>
       ))}
+      {/* "+" Button */}
+      <button
+        onClick={handleAddButtonClick}
+        className="w-9 h-9 rounded-full flex items-center justify-center font-normal text-sm shadow-lg
+          bg-gradient-to-r from-primary to-secondary text-white
+          focus:outline-none focus:ring-2 focus:ring-secondary"
+        aria-label="Add Member"
+      >
+        <IoIosAdd className="text-2xl" />
+      </button>
     </div>
   );
 };
