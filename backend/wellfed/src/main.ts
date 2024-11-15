4bed8da0794662220aeedd4de4baa440a1be9165
rithvikvelapati <rithvik.velapati@thehealthvice.com>
2024-11-15 09:40:31 -0600

    Resolved merge conflict in backend/wellfed/src/main.ts

--- backend/wellfed/src/main.ts
+++ backend/wellfed/src/main.ts
@@ -4,11 +4,13 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
   await app.listen(3001);
+
 }
 bootstrap();
